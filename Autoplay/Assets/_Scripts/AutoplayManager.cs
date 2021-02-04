132768527106abf39182be65631a2d70e1bc4d0d
OllytingXin <42716913+OllytingXin@users.noreply.github.com>
2021-02-04 00:56:18 -0500

    initialized

---
+++ Autoplay/Assets/_Scripts/AutoplayManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class AutoplayManager : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField]
+    private GameObject dialogUI = default;
+    [SerializeField]
+    private GameObject backgroundUI = default;
+    [SerializeField]
+    private GameObject[] charactersUI = default;
+
+    [Header("Files")]
+    [SerializeField]
+    private TextAsset[] txtFiles = default;
+}
