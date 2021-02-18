aa6969a76d4e91d0c39bff2e29eebbbf6826c428
OllytingXin <42716913+OllytingXin@users.noreply.github.com>
2021-02-18 05:45:18 -0500

    import character sprites

--- Autoplay/Assets/_Scripts/CharacterDissolveController.cs
+++ Autoplay/Assets/_Scripts/CharacterDissolveController.cs
@@ -10,6 +10,10 @@ public class CharacterDissolveController : MonoBehaviour
     bool isDissolving = false;
 
     float fade = 1f;
+    float cutOffHeight = 7.8f;
+
+    private GameObject scatterIns = default;
+    public Transform startPos = default;
 
     private void Start()
     {
@@ -24,21 +28,39 @@ public class CharacterDissolveController : MonoBehaviour
             StartCoroutine(DelayedInstantiate(scatterPS, 0.15f));
         }
 
-        if(isDissolving)
+        if (isDissolving)
         {
             fade -= Time.deltaTime;
-            if(fade <= 0f)
+            if (fade <= 0f)
             {
-                fade = 0f; 
+                fade = 0f;
                 isDissolving = false;
             }
             material.SetFloat("_Fade", fade);
         }
+
+        //if(isDissolving)
+        //{
+        //    cutOffHeight -= Time.deltaTime;
+        //    if(cutOffHeight < -7f)
+        //    {
+        //        cutOffHeight = -10.0f;
+        //        isDissolving = false;
+        //        Destroy(scatterIns.gameObject, 0.5f);
+        //    }
+
+        //    material.SetFloat("_CutoffHeight", cutOffHeight);
+        //    if(scatterIns)
+        //    {
+        //        scatterIns.transform.position += Vector3.up * Time.deltaTime * 0.65f;
+        //    }
+
+        //}
     }
 
     private IEnumerator DelayedInstantiate(GameObject _go, float delayTime)
     {
         yield return new WaitForSeconds(delayTime);
-        GameObject go = Instantiate(_go, transform.position, Quaternion.identity);
+        scatterIns = Instantiate(_go, startPos.position, Quaternion.identity);
     }
 }
