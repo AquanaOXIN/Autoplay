c9ce32c4eec1dffe233dbfff6a2b95496aa8a333
OllytingXin <42716913+OllytingXin@users.noreply.github.com>
2021-03-02 03:45:12 -0500

    UI physics

---
+++ Autoplay/Assets/DiceSpawnTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceSpawnTest : MonoBehaviour
+{
+    public GameObject diePrefab;
+
+    public void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GenDice(diePrefab);
+        }
+    }
+
+    private void GenDice(GameObject _go)
+    {
+        Color newColor = new Color(Random.Range(0.5f, 1f), Random.Range(0.5f, 1f), Random.Range(0.5f, 1f));
+        GameObject dieIns = Instantiate(_go, transform.position, Quaternion.identity);
+        dieIns.transform.SetParent(transform);
+        dieIns.GetComponent<Image>().color = newColor;
+        dieIns.GetComponent<RectTransform>().localScale *= Random.Range(0.001f, 0.01f);
+        Destroy(dieIns, 10f);
+    }
+}
