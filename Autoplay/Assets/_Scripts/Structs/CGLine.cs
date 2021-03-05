2bf387821cd502b75659f21e4344c1681d618280
Olly <ox7313@rit.edu>
2021-03-04 23:25:06 -0500

    3/4 modifications
    - implemented CG tag
    - change the way of UI characters slide out
    - increase the alpha of the stand by characters
    - delete the KP "silence emotion" feature

---
+++ Autoplay/Assets/_Scripts/Structs/CGLine.cs
@@ -0,0 +1,15 @@
+﻿// For Setup the CG
+
+public struct CGLine
+{
+    public string tagName;
+    public int? cgSelect;
+    public int? status;
+
+    public CGLine(string _tagName, int? _cgSelect, int? status)
+    {
+        this.tagName = _tagName;
+        this.cgSelect = _cgSelect;
+        this.status = status;
+    }
+}
